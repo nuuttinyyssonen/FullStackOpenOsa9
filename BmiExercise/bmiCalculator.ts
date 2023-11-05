85c65ef63fe5c1b0ae0117235c3c400e5e18f146
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2023-11-05 12:50:20 +0200

    exercises 9.7

--- BmiExercise/bmiCalculator.ts
+++ BmiExercise/bmiCalculator.ts
@@ -1,45 +1,45 @@
-const parseArgumentsBmi = (args: String[]) => {
-    if(args.length > 4) throw new Error('Too many arguments')
-    if(args.length < 4) throw new Error('Not enough arguments')
+const parseArgumentsBmi = (args: string[]) => {
+    if(args.length > 4) throw new Error('Too many arguments');
+    if(args.length < 4) throw new Error('Not enough arguments');
     if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
         return {
             height: Number(args[2]),
             weight: Number(args[3])
-        }
+        };
     } else {
-        throw new Error('Provided values were not numbers!')
+        throw new Error('Provided values were not numbers!');
     }
-}
+};
 
 export const calculateBmi = (height: number, weight: number) => {
-    const dividedHeight = height / 100
-    const bmi = weight/(dividedHeight*dividedHeight)
+    const dividedHeight = height / 100;
+    const bmi = weight/(dividedHeight*dividedHeight);
     if(bmi <= 16) {
-        return "Underweight (Severe thinness)"
+        return "Underweight (Severe thinness)";
     } else if(bmi >= 16 && bmi <= 16.9) {
-        return "Underweight (Moderate thinness)"
+        return "Underweight (Moderate thinness)";
     } else if(bmi >= 17 && bmi <= 18.4) {
-        return "Underweight (Mild thinness)"
+        return "Underweight (Mild thinness)";
     } else if(bmi >= 18.5 && bmi <= 24.9) {
-        return "Normal range"
+        return "Normal range";
     } else if(bmi >= 25 && bmi <= 29.9) {
-        return "Overweight (Pre-obese)"
+        return "Overweight (Pre-obese)";
     } else if(bmi >= 30 && bmi <= 34.9) {
-        return "Obese (Class I)"
+        return "Obese (Class I)";
     } else if(bmi >= 35 && bmi <= 39.9) {
-        return "Obese (Class II)"
+        return "Obese (Class II)";
     } else {
-        return "Obese (Class III)"
+        return "Obese (Class III)";
     }
-}
+};
 
 try {
     const { height, weight } = parseArgumentsBmi(process.argv);
     console.log(calculateBmi(height, weight));
 } catch(error: unknown) {
-    let errorMessage = 'Something bad happend'
+    let errorMessage = 'Something bad happend';
     if(error instanceof Error) {
-        errorMessage += error.message
+        errorMessage += error.message;
     }
-    console.log(errorMessage)
+    console.log(errorMessage);
 }
--- BmiExercise/index.ts
+++ BmiExercise/index.ts
@@ -1,27 +1,46 @@
-import express from 'express'
+import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
-    res.send('Hello Full Stack')
-})
+    res.send('Hello Full Stack');
+});
 
-app.get('/bmi/:height/:weight', (req, res) => {
-    if(isNaN(Number(req.params.height)) || isNaN(Number(req.params.weight))) {
-        return res.json({"error": "malfromatted parameters"})
+app.get('/bmi/', (req, res) => {
+    const height = req.query.height;
+    const weight = req.query.weight;
+    if(isNaN(Number(height)) || isNaN(Number(weight))) {
+        return res.status(400).send({"error": "malfromatted parameters"});
     }
-    const total = calculateBmi(Number(req.params.height), Number(req.params.weight))
+    const total = calculateBmi(Number(height), Number(weight));
     const result = {
-        weight: req.params.weight,
-        height: req.params.height,
+        weight: weight,
+        height: height,
         bmi: total
-    }
-    return res.json(result)
-})
+    };
+    return res.json(result);
+});
 
-const PORT = 3003
+app.post('/exercises', (req, res) => {
+    const daily_exercises = req.body.daily_exercises as number[];
+    const target = req.body.target as number;
+    if(daily_exercises.length !== 7 || !target) {
+        return res.status(400).send({"error": "parameters missing"});
+    }
+    for(let i = 0; i < daily_exercises.length; i++) {
+        if(typeof daily_exercises[i] !== 'number' || typeof target !== 'number') {
+            return res.status(400).send({"error": "malfromatted parameters"});
+        }
+    }
+    const result = calculateExercises(target, daily_exercises);
+    return res.send({ result });
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
